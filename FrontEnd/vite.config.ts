1c56610883d8fafaa3df64457eb888167652de47
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2025-02-18 11:04:58 +0530

    FrontEnd inti

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),eslint()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
