6685fd509bbfa2f6a9af3a9359530cdca86a98e5
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2025-02-20 19:10:35 +0530

    BackEnd

--- BackEnd/constants.py
+++ BackEnd/constants.py
@@ -3,9 +3,7 @@ import os
 load_dotenv()
 
 SERVER_URL = 'localhost'
-PORT = '8000'
+PORT = '8900'
 ENV = 'dev'
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY not found in environment variables")
